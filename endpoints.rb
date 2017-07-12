c61801b026bc73117369471589120e093f74e4fd
Ilya Noskov <ilya.noskov@sumup.com>
2017-07-12 16:20:15 +0300

    adds experimental button support

--- endpoints.rb
+++ endpoints.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 post '/api/interactions' do
   "Hello World"
@@ -14,5 +15,44 @@ end
 
 post '/api/lal' do
   puts request.body
-  "thanks"
+  content_type :json
+  {
+    "text": "What do you want to do?",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+  }.to_json
 end
