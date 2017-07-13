6bad0691bfd1e836dc139a6bbb1f439a73438072
Ilya Noskov <ilya.noskov@sumup.com>
2017-07-13 14:31:33 +0300

    adds user and admin jsons

--- commands/lal.rb
+++ commands/lal.rb
@@ -2,44 +2,18 @@ require 'json'
 
 class API < Sinatra::Base
     def lal
-         {
-      "attachments": [
-        {
-          "text": "What do you want to do?",
-          "fallback": "You are unable to choose a game",
-          "callback_id": "wopr_game",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "game",
-              "text": "Chess",
-              "type": "button",
-              "value": "chess"
-            },
-            {
-              "name": "game",
-              "text": "Falken's Maze",
-              "type": "button",
-              "value": "maze"
-            },
-            {
-              "name": "game",
-              "text": "Thermonuclear War",
-              "style": "danger",
-              "type": "button",
-              "value": "war",
-              "confirm": {
-                "title": "Are you sure?",
-                "text": "Wouldn't you prefer a good game of chess?",
-                "ok_text": "Yes",
-                "dismiss_text": "No"
-              }
-            }
-          ]
-        }
-      ]
-    }.to_json    
+      lal_admin
+      #check if user is admin
+      # all lal-admin or lal-user respectively
     end
-   
+
+    def lal_admin
+      admin_message = File.read('./commands/lal-admin.json')
+      admin_message.to_json
+    end
+
+    def lal_user
+      user_message = File.read('./commands/lal-user.json')
+      user_message.to_json
+    end
 end
