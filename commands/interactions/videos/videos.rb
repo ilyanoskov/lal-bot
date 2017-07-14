8c9a1d872c968c92df3c36bc3368c09e9e55839e
Ilya Noskov <ilya.noskov@sumup.com>
2017-07-14 18:33:35 +0300

    experimenting with unwatched videos

--- commands/interactions/videos/videos.rb
+++ commands/interactions/videos/videos.rb
@@ -2,9 +2,9 @@ require './db/objects/video.rb'
 
 class API < Sinatra::Base
     def get_unwatched_videos
-        Video.find_by(watched: false).each do |vid|
+        Video.where(watched: false).each do |vid|
             logger.info(vid)
         end
         videos
     end
-end
+en
