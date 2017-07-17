d003ba55a45b6d29374f27d748c39fce174f6504
Ilya Noskov <ilya.noskov@sumup.com>
2017-07-17 10:48:00 +0300

    video retrieval tool test

--- commands/interactions/videos/videos.rb
+++ commands/interactions/videos/videos.rb
@@ -7,11 +7,13 @@ class API < Sinatra::Base
         ]}
         Video.where(watched: false).each do |vid|
             logger.info(vid)
-            response[:attachments].push({
-                     title: vid.title,
-                     text: vid.description,
-                     title_link: vid.url,
-                     image_url: vid.thumbnail_small})
+            response[:attachments].push(
+           {
+               title: vid.title,
+               text: vid.description,
+               title_link: vid.url,
+               image_url: vid.thumbnail_small
+           })
         end
         response.to_json
     end
