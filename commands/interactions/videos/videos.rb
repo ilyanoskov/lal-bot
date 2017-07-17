cd883a948e0668c1f74f430e1f021ce495e7e7f7
Ilya Noskov <ilya.noskov@sumup.com>
2017-07-17 10:38:45 +0300

    video retrieval tool test

--- commands/interactions/videos/videos.rb
+++ commands/interactions/videos/videos.rb
@@ -2,8 +2,13 @@ require './db/objects/video.rb'
 
 class API < Sinatra::Base
     def get_unwatched_videos
+        response = {attachments: [
+            text: "Here are the videos I've got in the archives!"
+        ]}
         Video.where(watched: false).each do |vid|
             logger.info(vid)
+            response[:attachments].push({text: video.title})
         end
+        response.to_json
     end
 end
